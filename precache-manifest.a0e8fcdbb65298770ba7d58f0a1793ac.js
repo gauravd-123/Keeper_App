d901d8c50a6a64ad5950b590fe8a73e432f5b58a
Code Sandbox <hello@codesandbox.io>
2023-02-12 07:45:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a0e8fcdbb65298770ba7d58f0a1793ac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8306a054f0a53ce3c99e176019f586c5",
+    "url": "https://gauravd-123.github.io/csb-opi5s8/index.html"
+  },
+  {
+    "revision": "8f4489d49fd6bf7a7c3b",
+    "url": "https://gauravd-123.github.io/csb-opi5s8/static/js/2.391f220e.chunk.js"
+  },
+  {
+    "revision": "bf05ff9d8cdac4b87098",
+    "url": "https://gauravd-123.github.io/csb-opi5s8/static/js/main.30917b7e.chunk.js"
+  },
+  {
+    "revision": "9222cc0e5447724c4044",
+    "url": "https://gauravd-123.github.io/csb-opi5s8/static/js/runtime-main.d4af9c3f.js"
+  }
+]);
